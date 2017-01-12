eaf8f60fe9e36cdf50663c3d614eefd887b5c2c9
pallab-gain <pallab.gain@gmail.com>
2017-01-12 16:48:58 +0600

    Fixing segmentation fault

--- plugins/janus_videoroom.c
+++ plugins/janus_videoroom.c
@@ -2348,9 +2348,8 @@ void janus_videoroom_hangup_media(janus_plugin_session *handle) {
 
         /* Notify all listeners */
         char request[100];
-        if(participant->listeners) {
-            guint64 room_id = *((guint64 *)participant->room->room_id);
-            g_snprintf(request, sizeof(request), "{\"event\":\"unpublished\",\"transaction\":\"internal\",\"room\":%"SCNu64"}", room_id);
+        if(participant->listeners && participant->room) {
+            g_snprintf(request, sizeof(request), "{\"event\":\"unpublished\",\"transaction\":\"internal\",\"room\":%"SCNu64"}", participant->room->room_id);
             g_slist_foreach(participant->listeners, janus_videoroom_relay_data_packet, request);
         }
 
